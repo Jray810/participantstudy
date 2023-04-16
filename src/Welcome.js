009c6490991e35a7aa9ce2e0a991dbd0cdd08a97
Raymond Jia <jia.raymond19@gmail.com>
2023-04-15 23:06:25 -0400

    Added task data and screen restrictions

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,6 +1,6 @@
 // This is the Welcome State
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { setExpState } from './app/stateSlice';
 import { setGroup } from './app/participantGroupSlice';
@@ -18,6 +18,19 @@ function Welcome() {
     dispatch(setExpState('Instructions'));
   }
 
+  // Screen Width Restrictions
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleWindowResize);
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    }
+  }, [])
+
   return (
     <div>
       <div style={{paddingBottom: "0"}}>
@@ -27,7 +40,7 @@ function Welcome() {
       </div>
       <div style={{margin: "auto"}}>
         <div>
-          Thank you for volunteering to participate in this study. Please note that a <b>computer with a keyboard is highly recommended for completing this study</b> and that small mobile devices like smartphones may not render the system properly. Please make sure that you are setup properly before proceeding.
+          Thank you for volunteering to participate in this study. Please note that a <b>computer with a keyboard is highly recommended for completing this study</b>. Please make sure that you are setup properly before proceeding.
         </div>
         <div>
           This study must be completed in one sitting and is timed. The study is expected to take between <b>20</b> to <b>30</b> minutes total. Please make sure you are able to complete the study in one sitting and please do not refresh the webpage during the study.
@@ -35,9 +48,18 @@ function Welcome() {
         <div>
           When you are ready, click the button below to begin.
         </div>
-        <div>
-          <button onClick={() => begin()}>Begin</button>
-        </div>
+        {
+          windowWidth >= 1024 &&
+          <div>
+            <button onClick={() => begin()}>Begin</button>
+          </div>
+        }
+        {
+          windowWidth < 1024 &&
+          <div>
+            <p><b>Your screen width is too small, try rotating your screen. If this message does not change, please switch to a device with a screen width of at least 1024px.</b></p>
+          </div>
+        }
       </div>
     </div>
   );
--- src/data/taskData.js
+++ src/data/taskData.js
@@ -6,74 +6,102 @@ export const task_stats = {
 export const task_data = [
   {
     sample: 1,
-    f1: 0.1,
-    f2: 0,
+    f1: 0.7194,
+    f2: 0.1197,
     f3: 1,
-    class: 1,
+    f4: 0,
+    class: 0,
     calcExpr: -1,
-    imgPath: '/images/analogVis/1.png'
+    imgPath: ''
   },
   {
     sample: 2,
-    f1: 0.2,
-    f2: 1,
+    f1: 2.935,
+    f2: 1.3081,
     f3: 1,
-    class: 0,
+    f4: 1,
+    class: 1,
     calcExpr: -1,
-    imgPath: '/images/analogVis/2.png'
+    imgPath: ''
   },
   {
     sample: 3,
-    f1: 0.3,
-    f2: 0,
+    f1: 0.0852,
+    f2: 0.3619,
     f3: 0,
+    f4: 0,
     class: 0,
     calcExpr: -1,
-    imgPath: '/images/analogVis/3.png'
+    imgPath: ''
   },
   {
     sample: 4,
-    f1: 0.4,
-    f2: 1,
-    f3: 0,
-    class: 1,
+    f1: 0.9767,
+    f2: 2.0307,
+    f3: 1,
+    f4: 0,
+    class: 2,
     calcExpr: -1,
-    imgPath: '/images/analogVis/4.png'
+    imgPath: ''
   },
   {
     sample: 5,
-    f1: 0.5,
-    f2: 1,
-    f3: 1,
-    class: 1,
+    f1: 0.8904,
+    f2: 0.2276,
+    f3: 0,
+    f4: 1,
+    class: 2,
     calcExpr: -1,
-    imgPath: '/images/analogVis/5.png'
+    imgPath: ''
   },
   {
     sample: 6,
-    f1: 0.6,
-    f2: 0,
+    f1: 2.3822,
+    f2: 0.7848,
     f3: 1,
-    class: 2,
+    f4: 1,
+    class: 0,
     calcExpr: -1,
-    imgPath: '/images/analogVis/6.png'
+    imgPath: ''
   },
   {
     sample: 7,
-    f1: 0.7,
-    f2: 1,
-    f3: 1,
-    class: 1,
+    f1: 0.0274,
+    f2: 0.3195,
+    f3: 0,
+    f4: 1,
+    class: 0,
     calcExpr: -1,
-    imgPath: '/images/analogVis/7.png'
+    imgPath: ''
   },
   {
     sample: 8,
-    f1: 0.8,
-    f2: 0,
+    f1: 1.1125,
+    f2: 0.7045,
     f3: 1,
+    f4: 0,
     class: 2,
     calcExpr: -1,
-    imgPath: '/images/analogVis/8.png'
+    imgPath: ''
+  },
+  {
+    sample: 9,
+    f1: 1.1357,
+    f2: 2.5421,
+    f3: 0,
+    f4: 1,
+    class: 1,
+    calcExpr: -1,
+    imgPath: ''
+  },
+  {
+    sample: 10,
+    f1: 2.4343,
+    f2: 2.7953,
+    f3: 1,
+    f4: 0,
+    class: 1,
+    calcExpr: -1,
+    imgPath: ''
   }
 ]
